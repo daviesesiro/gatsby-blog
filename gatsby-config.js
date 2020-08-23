10b330eedc777ee16f1be3deb7023981d1275cc4
Davies Esiro <ojurereoluwad@gmail.com>
2020-08-23 03:50:00 +0100

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `My Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Davies Esiro`,
-    pathPrefix: "/reponame",
+    pathPrefix: "/gatsby-blog",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
